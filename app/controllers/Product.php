e75b844453b0fc478a23402b9eb54f8b3dff4e6b
Wanderson <emailwandersonalves@gmail.com>
2025-03-13 08:39:15 -0300

    Exibindo o controller home caso não encontre o controller solicitado

--- app/controllers/Product.php
+++ app/controllers/Product.php
@@ -4,6 +4,11 @@ namespace app\controllers;
 
 class Product
 {
+    public array $data = [
+        'title' => 'Product'
+    ];
+    public string $view = 'product.php';
+
     public function index(){
     }
 
--- app/core/ControllerExtract.php
+++ app/core/ControllerExtract.php
@@ -6,21 +6,16 @@ class ControllerExtract
 {
     public static function extract()
     {
-        $controller = 'Home';
         $uri = Uri::uri();
                 
         if(!empty($uri[0])){
-            $controller = ucfirst($uri[0]);
+            $namespaceAndController = "app\\controllers\\" . ucfirst($uri[0]);
+            
+            if(class_exists($namespaceAndController)){
+                return $namespaceAndController;
+            }
         }
 
-        $namespaceAndController = "app\\controllers\\{$controller}";
-
-        if(class_exists($namespaceAndController)){
-            $controller = $namespaceAndController;
-        } else {
-            $controller = "app\\controllers\\{$controller}";
-        }
-
-        return $controller;
+        return "app\\controllers\\Home";
     }
 }
