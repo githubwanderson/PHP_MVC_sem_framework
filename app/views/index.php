1f4b9c05e0562e3c1c3903637ae21ad034341f99
Wanderson <emailwandersonalves@gmail.com>
2025-04-01 08:51:50 -0300

    Adicionado css

--- app/views/index.php
+++ app/views/index.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    
     <title><?php echo $title ?></title>
 </head>
-<body>
-    <h2>MVC sem framework</h2>
-
-    <div>
+<body>    
+    <div class="container">
+        <h2>MVC sem framework</h2>
         <?php require VIEW_PATH . $this->controller->view; ?>
     </div>
 </body>
